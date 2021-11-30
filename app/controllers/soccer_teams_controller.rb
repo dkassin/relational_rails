14984710fcb85e6329ca83e69d593131a9860639
David Kassin <david.j.kassin@gmail.com>
2021-11-29 17:52:37 -0700

    Add forms for both tables and routes

--- app/controllers/soccer_teams_controller.rb
+++ app/controllers/soccer_teams_controller.rb
@@ -2,4 +2,10 @@ class SoccerTeamsController < ApplicationController
   def index
     @soccer_teams = ['Soccer Team 1', 'Soccer Team 2', 'Soccer Team 3']
   end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/soccer_teams', to: 'soccer_teams#index'
   get '/mountain_ranges', to: 'mountain_ranges#index'
+  get '/soccer_teams/new', to: 'soccer_teams#new'
+  get '/mountain_ranges/new', to: 'mountain_ranges#new'
+  post '/soccer_teams', to: 'soccer_teams#create'
+  post '/mountain_ranges', to: 'mountain_ranges#create'
 end
