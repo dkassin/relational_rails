bae19476a723306cd300bb76db0aa5eba9c3eb3a
David Kassin <david.j.kassin@gmail.com>
2021-11-29 17:23:53 -0700

    setup webpage and index for soccer team and mountain ranges

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
+  get '/soccer_teams', to: 'soccer_teams#index'
+  get '/mountain_ranges', to: 'mountain_ranges#index'
 end
