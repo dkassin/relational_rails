c4aff52059205c99a0311efb89b9152144a2a59a
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-12-02 14:17:26 -0700

    Test: story 10 complete for soccer team

--- spec/features/soccer_teams/show_spec.rb
+++ spec/features/soccer_teams/show_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'the soccer teams show web page' do
     @rob_c = @bar.soccer_players.create!(name: 'Rob C', homegrown_player: true, games_played: 64)
     @jose_r = @bar.soccer_players.create!(name: 'Jose R', homegrown_player: false, games_played: 14)
   end
-  
+
   it "will show the soccer teams attributes" do
 
     visit "/soccer_teams/#{@rm.id}"
@@ -29,5 +29,12 @@ RSpec.describe 'the soccer teams show web page' do
     visit "/soccer_teams/#{@rm.id}"
 
     expect(page).to have_content(@rm.player_count)
+  end
+
+  it "gives a link to go to the soccer team's soccer players page" do
+    visit "/soccer_teams/#{@rm.id}"
+
+    click_on "Soccer Players on Team"
+    expect(current_path).to eq ("/soccer_teams/#{@rm.id}/soccer_players")
   end
 end
