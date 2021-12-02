57ef7ccaa633e6256d3197d6ef909e4c1596f144
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-12-01 17:44:18 -0700

    Test: story 2 adjust for mountain range

--- spec/features/mountain_range/show_spec.rb
+++ spec/features/mountain_range/show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'the mountain ranges show web page' do
   it "will show the mountain range and its attributes" do
     rocky = MountainRange.create!(name: "Rocky Mountains", has_ski_resort: true, included_states: 4)
-    appalachian = MountainRange.create!(name: "Appalachian Mountains", has_ski_resort: true, included_states: 12)
+    appalachian = MountainRange.create!(name: "Appalachian Mountains", has_ski_resort: false, included_states: 12)
 
     #localhost:3000/mountain_ranges/4
     visit "/mountain_ranges/#{rocky.id}"
