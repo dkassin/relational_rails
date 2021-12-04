5a25356372436625b133399d3bed332a6f1c1707
David Kassin <david.j.kassin@gmail.com>
2021-12-03 17:33:39 -0700

    Story 13 work in progress

--- app/controllers/soccer_team_soccer_players_controller.rb
+++ app/controllers/soccer_team_soccer_players_controller.rb
@@ -3,4 +3,21 @@ class SoccerTeamSoccerPlayersController < ApplicationController
     @soccer_team = SoccerTeam.find(params[:id])
     @soccer_players = @soccer_team.soccer_players
   end
+
+  def new
+  end
+
+  def create
+    @soccer_team = SoccerTeam.find(params[:id])
+    soccer_player = SoccerPlayer.new({
+      name: params[:soccer_players][:name],
+      homegrown_player: params[:soccer_players][:homegrown_player],
+      games_played: params[:soccer_players][:games_played],
+      soccer_teams_id: params[:soccer_players][:soccer_teams_id]
+      })
+
+    soccer_player.save
+    redirect_to "/soccer_teams/#{@soccer_team.id}/soccer_players"
+  end
+
 end
---
+++ spec/features/soccer_teams/soccer_players/new_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe 'New Soccer Player' do
+  before :each do
+    @rm = SoccerTeam.create!(name: 'Real Madrid', qualified_champ_league: true, points: 23)
+    @bar = SoccerTeam.create!(name: 'Barcelona', qualified_champ_league: true, points: 13)
+    @cad = SoccerTeam.create!(name: 'Cadiz CF', qualified_champ_league: false, points: 22)
+    @bet = SoccerTeam.create!(name: 'Real Betis', qualified_champ_league: false, points: 11)
+    @am = SoccerTeam.create!(name: 'Aletico Madrid', qualified_champ_league: true, points: 28)
+
+    @jimmy_g = @rm.soccer_players.create!(name: 'Jimmy G', homegrown_player: false, games_played: 23)
+    @jay_c = @rm.soccer_players.create!(name: 'Jay C', homegrown_player: true, games_played: 67)
+
+    @cay_c = @bar.soccer_players.create!(name: 'Cay C', homegrown_player: true, games_played: 86)
+    @rob_c = @bar.soccer_players.create!(name: 'Rob C', homegrown_player: true, games_played: 64)
+    @jose_r = @bar.soccer_players.create!(name: 'Jose R', homegrown_player: false, games_played: 14)
+
+    @luis_c = @cad.soccer_players.create!(name: 'Luis C', homegrown_player: false, games_played: 14)
+    @romeo_f = @cad.soccer_players.create!(name: 'Romeo F', homegrown_player: true, games_played: 34)
+    @tim_g = @cad.soccer_players.create!(name: 'Tim G', homegrown_player: false, games_played: 124)
+    @jun_f = @bet.soccer_players.create!(name: 'Jun F', homegrown_player: true, games_played: 202)
+
+    @sam_g = @am.soccer_players.create!(name: 'Sam G', homegrown_player: false, games_played: 224)
+    @ron_l = @am.soccer_players.create!(name: 'Ron L', homegrown_player: true, games_played: 104)
+  end
+
+  describe 'As a visitor' do
+    describe ' When I visit the soccer teams, soccer player index there is a link' do
+      it 'I can create a new soccer player for that team' do
+        visit "/soccer_teams/#{@cad.id}/soccer_players"
+
+        click_link 'Add another Soccer Player'
+
+        expect(current_path).to eq("/soccer_teams/#{@cad.id}/soccer_players/new")
+
+        fill_in('name', with: 'Chris H')
+        fill_in('homegrown_player', with: true)
+        fill_in('games_played', with: 88)
+
+        click_on('Create Soccer Player')
+
+
+        expect(current_path).to eq("//soccer_teams/#{@cad.id}/soccer_players")
+        expect(page).to have_content("Chris H")
+        expect(page).to have_content(true)
+        expect(page).to have_content(88)
+      end
+    end
+  end
+end
